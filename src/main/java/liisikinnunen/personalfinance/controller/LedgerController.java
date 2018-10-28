811a8bb0ae0980845524cbe7ef08a766b2764812
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2018-10-28 22:56:46 +0200

    Create LedgerController file

---
+++ src/main/java/liisikinnunen/personalfinance/controller/LedgerController.java
@@ -0,0 +1,15 @@
+package liisikinnunen.personalfinance.controller;
+
+import liisikinnunen.personalfinance.model.Ledger;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static liisikinnunen.personalfinance.util.PathAndRelMappings.PATH_LEDGERS;
+
+@RestController
+public class LedgerController {
+}
