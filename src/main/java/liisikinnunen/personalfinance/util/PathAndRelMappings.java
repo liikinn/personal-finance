8647fd6e8174533651fd8319f2060edc5d75b9a6
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2018-10-28 23:03:02 +0200

    Add file for api endpoints.

---
+++ src/main/java/liisikinnunen/personalfinance/util/PathAndRelMappings.java
@@ -0,0 +1,6 @@
+package liisikinnunen.personalfinance.util;
+
+public class PathAndRelMappings {
+    public static final String PATH_LEDGERS = "ledgers";
+    public static final String PATH_USERS = "users";
+}
