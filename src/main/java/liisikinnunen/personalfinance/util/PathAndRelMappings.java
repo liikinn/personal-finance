6f659bf7ff88c01e396e01e64101de66d585ded9
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2019-01-02 00:18:51 +0200

    Add item resource and collection resource variables for ledger and user

--- src/main/java/liisikinnunen/personalfinance/util/PathAndRelMappings.java
+++ src/main/java/liisikinnunen/personalfinance/util/PathAndRelMappings.java
@@ -1,6 +1,13 @@
 package liisikinnunen.personalfinance.util;
 
 public class PathAndRelMappings {
+    // LEDGER
     public static final String PATH_LEDGERS = "ledgers";
+    public static final String REL_LEDGER_ITEM_RESOURCE = "ledger";
+    public static final String REL_LEDGER_COLLECTION_RESOURCE = "ledgers";
+
+    // USER
     public static final String PATH_USERS = "users";
+    public static final String REL_USER_ITEM_RESOURCE = "user";
+    public static final String REL_USER_COLLECTION_RESOURCE = "users";
 }
