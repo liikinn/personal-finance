33e76c8be17809edd94a8c080145c839079c16d6
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2019-01-02 00:36:11 +0200

    Add basic structure for ledger repository

--- src/main/java/liisikinnunen/personalfinance/repository/LedgerRepository.java
+++ src/main/java/liisikinnunen/personalfinance/repository/LedgerRepository.java
@@ -1,13 +1,13 @@
 package liisikinnunen.personalfinance.repository;
 
 import liisikinnunen.personalfinance.model.Ledger;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import static liisikinnunen.personalfinance.util.PathAndRelMappings.PATH_LEDGERS;
+
+import static liisikinnunen.personalfinance.util.PathAndRelMappings.*;
 
 @RepositoryRestResource(
-        path = PATH_LEDGERS
-)
-public interface LedgerRepository extends CrudRepository<Ledger, Long> {
+        path = PATH_LEDGERS,
+        itemResourceRel = REL_LEDGER_ITEM_RESOURCE,
+        collectionResourceRel = REL_LEDGER_COLLECTION_RESOURCE)
+public interface LedgerRepository extends ExtendedCrudRepository<Ledger, Long> {
 }
