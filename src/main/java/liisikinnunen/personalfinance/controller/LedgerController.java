1662e4437763bf1d5c232dedddaba6d3c79a46dd
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2019-01-02 00:31:15 +0200

    Add base structure for LedgerController

--- src/main/java/liisikinnunen/personalfinance/controller/LedgerController.java
+++ src/main/java/liisikinnunen/personalfinance/controller/LedgerController.java
@@ -1,15 +1,9 @@
 package liisikinnunen.personalfinance.controller;
 
-import liisikinnunen.personalfinance.model.Ledger;
-import org.springframework.hateoas.ResourceSupport;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.webmvc.BasePathAwareController;
 
-import java.util.List;
-
-import static liisikinnunen.personalfinance.util.PathAndRelMappings.PATH_LEDGERS;
-
-@RestController
+@BasePathAwareController
+@RequiredArgsConstructor
 public class LedgerController {
 }
