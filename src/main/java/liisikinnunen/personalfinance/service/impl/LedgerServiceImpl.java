44142cc9471fcfa7fdbd67f19fbbc11caa4415a8
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2019-01-02 00:37:56 +0200

    Add basic ledger service structure

--- src/main/java/liisikinnunen/personalfinance/service/impl/LedgerServiceImpl.java
+++ src/main/java/liisikinnunen/personalfinance/service/impl/LedgerServiceImpl.java
@@ -2,9 +2,13 @@ package liisikinnunen.personalfinance.service.impl;
 
 import liisikinnunen.personalfinance.repository.LedgerRepository;
 import liisikinnunen.personalfinance.service.LedgerService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service("ledgerService")
+@RequiredArgsConstructor
 public class LedgerServiceImpl implements LedgerService {
+    @NonNull
     private final LedgerRepository ledgerRepository;
 }
