973f3e879bc26f62aa2f0013009ca6ee9b07dd36
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2019-01-03 00:32:33 +0200

    Add links to returned responses.

--- src/main/java/liisikinnunen/personalfinance/controller/UserController.java
+++ src/main/java/liisikinnunen/personalfinance/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.rest.webmvc.*;
 import org.springframework.data.rest.webmvc.json.DomainObjectReader;
 import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.UriTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static liisikinnunen.personalfinance.util.PathAndRelMappings.PATH_USERS;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @BasePathAwareController
 @RequiredArgsConstructor
@@ -46,13 +48,21 @@ public class UserController {
     @GetMapping(PATH_USERS)
     public ResponseEntity<ResourceSupport> getAllUsers() {
         List<User> users = userService.getAllUsers();
-        return controllerUtil.collectionToResponseEntity(users, null);
+        return controllerUtil.collectionToResponseEntity(users, methodOn(this.getClass()).getAllUsers(), null);
+    }
+
+    private void addLocationHeader(HttpHeaders headers, PersistentEntityResourceAssembler assembler, Object source) {
+        String selfLink = assembler.getSelfLinkFor(source).getHref();
+        headers.setLocation(new UriTemplate(selfLink).expand());
     }
 
     private ResponseEntity<ResourceSupport> returnResponseEntity(PersistentEntityResourceAssembler resourceAssembler,
-                                                                 User user, HttpStatus httpStatus) {
+                                                                 User user, HttpStatus httpStatus, boolean addLocation) {
         PersistentEntityResource resource = resourceAssembler.toFullResource(user);
         HttpHeaders headers = headersPreparer.prepareHeaders(Optional.of(resource));
+        if (addLocation) {
+            addLocationHeader(headers, resourceAssembler, user);
+        }
 
         switch (httpStatus) {
             case NO_CONTENT:
--- src/main/java/liisikinnunen/personalfinance/util/ControllerUtil.java
+++ src/main/java/liisikinnunen/personalfinance/util/ControllerUtil.java
@@ -2,19 +2,27 @@ package liisikinnunen.personalfinance.util;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.ControllerUtils;
 import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.core.DummyInvocationUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URL;
 import java.util.Collection;
 import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @Component
 @RequiredArgsConstructor
@@ -22,11 +30,25 @@ public class ControllerUtil {
     @NonNull
     private RepositoryRestConfiguration config;
 
-    public ResponseEntity<ResourceSupport> collectionToResponseEntity(Collection<?> collection, PersistentEntityResourceAssembler assembler) {
+    public ResponseEntity<ResourceSupport> collectionToResponseEntity(Collection<?> collection, Object selfLinkTarget, PersistentEntityResourceAssembler assembler) {
+        Link selfLink = toBasePathAwareLink(selfLinkTarget).withSelfRel();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        Resources<?> resources = new Resources<>(toContent(collection, assembler));
+        Resources<?> resources = new Resources<>(toContent(collection, assembler), selfLink);
         return ControllerUtils.toResponseEntity(HttpStatus.OK, headers, resources);
+    }
+
+    private Link toBasePathAwareLink(Object selfLinkTarget) {
+        Assert.isInstanceOf(DummyInvocationUtils.LastInvocationAware.class, selfLinkTarget);
+        return fixLinkTo((DummyInvocationUtils.LastInvocationAware) selfLinkTarget);
+    }
+
+    @SneakyThrows
+    private Link fixLinkTo(DummyInvocationUtils.LastInvocationAware invocationValue) {
+        UriComponentsBuilder uriComponentsBuilder = linkTo(invocationValue).toUriComponentsBuilder();
+        URL url = new URL(uriComponentsBuilder.toUriString());
+        uriComponentsBuilder.replacePath(config.getBasePath() + url.getPath());
+        return new Link(uriComponentsBuilder.toUriString());
     }
 
     private Collection<?> toContent(Collection<?> collection, PersistentEntityResourceAssembler assembler) {
